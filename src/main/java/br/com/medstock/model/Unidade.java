4115a9fa9c13f96a039ca4a8f6253fd672382a8f
Jean Matheus <jeanmatheus0612@gmail.com>
2025-06-12 19:27:06 -0300

    Criando e estruturando a classe Unidade

---
+++ src/main/java/br/com/medstock/model/Unidade.java
@@ -0,0 +1,27 @@
+package br.com.medstock.model;
+
+public class Unidade {
+    private String nome;
+    private String localizacao;
+
+    public Unidade(String nome, String localizacao) {
+        this.nome = nome;
+        this.localizacao = localizacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+}
